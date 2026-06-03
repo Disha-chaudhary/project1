6f34a177226af8f1fab3ca1db08f7bb0bf74d255
Disha Chaudhary <dishachaudhary129@gmail.com>
2026-06-04 00:03:25 +0530

    interview apisuccessfull

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -22,8 +22,10 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 console.log("✅ app.js loaded");
 
