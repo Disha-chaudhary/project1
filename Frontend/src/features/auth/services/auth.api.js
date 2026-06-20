a7110092bc62cd008c2d61d082c6f3bdc5272a8b
Disha Chaudhary <dishachaudhary129@gmail.com>
2026-06-20 22:43:02 +0530

    update frontend api urls for deployment

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-export async function  register(name, email, password) {
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+
+export async function register(name, email, password) {
     try {
-        const response = await axios.post('http://localhost:3000/api/auth/register', {
+        const response = await axios.post(`${API_URL}/api/auth/register`, {
             name,
             email,  
             password
@@ -19,7 +21,7 @@ export async function  register(name, email, password) {
 
 export async function login(email, password) {
     try {
-        const response = await axios.post('http://localhost:3000/api/auth/login', {
+        const response = await axios.post(`${API_URL}/api/auth/login`, {
             email,
             password
         },{
@@ -35,7 +37,7 @@ export async function login(email, password) {
 
 export async function logout() {
     try {
-        const response = await axios.get('http://localhost:3000/api/auth/logout', {
+        const response = await axios.get(`${API_URL}/api/auth/logout`, {
             withCredentials: true
         });
         return response.data;
@@ -45,9 +47,10 @@ export async function logout() {
         throw error;
     }
 }
+
 export async function getMe() {
     try {
-        const response = await axios.get('http://localhost:3000/api/auth/get-me', {
+        const response = await axios.get(`${API_URL}/api/auth/get-me`, {
             withCredentials: true
         });
         return response.data;
